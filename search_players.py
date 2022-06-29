e7f264e505d1544217dab923a77c6728646d9b5c
Niko Honu <nikohonu@gmail.com>
2022-06-29 09:55:55 +0300

    improve search_player.py & generating basic tables

--- search_players.py
+++ search_players.py
@@ -12,7 +12,7 @@ START = 76561198083927293
 END = 76561198899999999
 steam_ids = nh_tools.file.open_json(pathlib.Path('users.json'), [])
 steam_ids = set(steam_ids)
-while len(steam_ids) < 200:
+while len(steam_ids) < 1000:
     steam_id = random.randrange(START, END)
     time.sleep(4)
     try:
@@ -21,8 +21,7 @@ while len(steam_ids) < 200:
         result = requests.get(url)
         result = result.json()
         steam_ids.add(steam_id)
+        nh_tools.file.save_json(pathlib.Path('users.json'), list(steam_ids))
         print('ok')
     except json.decoder.JSONDecodeError:
         continue
-print(steam_ids)
-nh_tools.file.save_json(pathlib.Path('users.json'), list(steam_ids))
