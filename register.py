6de97707e8304e398b83b1843429cead99226b31
nikohonu <nikohonu@gmail.com>
2022-08-17 15:52:10 +0300

    improve register.py script

--- register.py
+++ register.py
@@ -3,26 +3,54 @@ import json
 import pathlib
 import random
 import time
+from urllib import response
 
 import requests
 
+START = 76561198083927293
+END = 76561198899999999
+
 with pathlib.Path('steam_ids.json').open() as data_file:
     steam_ids = json.load(data_file)
-    random.shuffle(steam_ids)
 
+# check is steam_ids is registered
+i = 0
 for steam_id in steam_ids:
-    response = requests.get(f'http://127.0.0.1:5000/steam/get-profile/{steam_id}')
-    try:
-        print(response.url)
-        profile = response.json()
+    i += 1
+    if i == 150:
+        break
+    response = requests.get(
+        f'http://127.0.0.1:5000/steam/check-profile/{steam_id}')
+    if response.status_code != 404:
+        print(response.text)
+    else:
+        response = requests.get(
+            f'http://127.0.0.1:5000/steam/get-profile/{steam_id}')
+        if response.status_code:
+            print(response.text)
+            profile = response.json()
         response = requests.post(
             'http://127.0.0.1:5000/auth/register',
             {'username': profile['name'],
              'password': '42774277', 'steam_profile_url': profile['url']})
-        print(response)
         response = requests.get(
-            f'http://127.0.0.1:5000/steam/get-profile-apps/{profile["id"]}')
-        print(response)
-        time.sleep(3)
-    except json.decoder.JSONDecodeError as error:
-        print(f"Error: {error}")
+            f'http://127.0.0.1:5000/user/{profile["name"]}')
+        response = requests.get(
+            f'http://127.0.0.1:5000/user/{profile["name"]}/games')
+    response = requests.get(
+        f'http://127.0.0.1:5000/steam/check-profile/{steam_id}')
+    if response.status_code == 404:
+        response = requests.post(
+            'http://127.0.0.1:5000/auth/delete',
+            {'username': profile['name'], 'password': '42774277'})
+        i -= 1
+
+        # for steam_id in steam_ids:
+        #     try:
+        #         print(response)
+        #     except requests.exceptions.ConnectionError as error:
+        #         print(f"Error: {error}")
+        #         time.sleep(60)
+        #     except requests.exceptions.JSONDecodeError:
+        #         print(f"Error: {error}")
+        #         pass
